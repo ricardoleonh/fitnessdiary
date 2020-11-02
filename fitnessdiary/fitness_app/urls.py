4a54eae3821ca2bdf121446b8e08f25af903c056
ricardoleonh <67030936+ricardoleonh@users.noreply.github.com>
2020-11-02 07:27:31 -0600

    added urls

--- fitnessdiary/fitness_app/urls.py
+++ fitnessdiary/fitness_app/urls.py
@@ -2,4 +2,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
 path('', views.index),
+path('signin', views.signin),
+path('register', views.register_form),
+path('login', views.login),
+path('dashboard', views.dashboard),
+path('registration', views.registration),
+
 ]
