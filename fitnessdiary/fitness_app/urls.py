d9edb34ba61aab34fe4d134c102c733837d10efc
ricardoleonh <67030936+ricardoleonh@users.noreply.github.com>
2020-11-02 14:54:10 -0600

    added id to url

--- fitnessdiary/fitness_app/urls.py
+++ fitnessdiary/fitness_app/urls.py
@@ -5,7 +5,7 @@ path('', views.index),
 path('signin', views.signin),
 path('register', views.register_form),
 path('login', views.login),
-path('dashboard', views.dashboard),
+path('dashboard/<int:id>', views.dashboard),
 path('registration', views.registration),
 
 ]
