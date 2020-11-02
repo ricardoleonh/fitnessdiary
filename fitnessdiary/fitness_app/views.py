b96643f855cbb56a972d022175fc2a5493ae8318
ricardoleonh <67030936+ricardoleonh@users.noreply.github.com>
2020-11-02 07:43:14 -0600

    added views

--- fitnessdiary/fitness_app/views.py
+++ fitnessdiary/fitness_app/views.py
@@ -5,3 +5,54 @@ import bcrypt
 
 def index(request):
     return render(request, 'login.html')
+
+def signin(request):
+    return render(request, 'login.html')
+
+def register_form(request):
+    return render(request, 'registration.html')
+
+def login(request):
+    if request.method == 'POST':
+        errors = User.objects.log_validator(request.POST) #to validate the form is completed correctly
+        if len(errors) != 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect ('/')
+        this_user = User.objects.get(user_name=request.POST['user_name'])
+        request.session['user_user_name'] = this_user.user_name
+        request.session['user_first_name'] = this_user.first_name
+        request.session['user_last_name'] = this_user.last_name
+        request.session['user_email'] = this_user.email
+        request.session['user_id'] = this_user.id
+        return redirect('/dashboard')
+    return render(request, "dashboard.html") 
+
+def dashboard(request):
+    if 'user_id' not in request.session: #validate user is logged in
+        return redirect('/')
+    return render (request, 'dashboard.html')
+
+def registration(request):
+    if request.method == 'POST':
+        errors = User.objects.reg_validator(request.POST) #to validate the form is completed correctly
+        if len(errors) != 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect ('/register')
+        hashed_pw = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode() #to hash the password
+        new_user = User.objects.create ( #to create a new user
+            user_name = request.POST['user_name'],
+            first_name = request.POST['first_name'],
+            last_name = request.POST['last_name'],
+            email = request.POST['email'],
+            password = hashed_pw
+        )
+        request.session['user_user_name'] = new_user.user_name
+        request.session['user_first_name'] = new_user.first_name
+        request.session['user_first_name'] = new_user.first_name
+        request.session['user_last_name'] = new_user.last_name
+        request.session['user_email'] = new_user.email
+        request.session['user_id'] = new_user.id
+        return redirect ('/dashboard')
+    return redirect ('/')
