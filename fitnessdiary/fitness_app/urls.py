6144ff221664aaf2b0f57ad7bd31e5aaee49c1a7
ricardoleonh <67030936+ricardoleonh@users.noreply.github.com>
2020-11-03 08:57:28 -0600

    logout url

--- fitnessdiary/fitness_app/urls.py
+++ fitnessdiary/fitness_app/urls.py
@@ -9,4 +9,5 @@ path('dashboard', views.dashboard),
 path('registration', views.registration),
 path('myaccount/<int:id>', views.edit_account),
 path('back', views.back),
+path('logout', views.logout)
 ]
